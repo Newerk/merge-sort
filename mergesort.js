435a79ab260f24a4958b7bc73e9196df992611b3
newerk <juniusowens@yahoo.com>
2023-02-10 14:33:03 -0500

    .

--- mergesort.js
+++ mergesort.js
@@ -1,3 +1,7 @@
 //takes in an array and returns a sorted array, using a recursive merge sort methodology
 const mergeSort = (array) => {
+    let arrayCopy = [...array];
+    console.log(`this is a shallow copy: ${arrayCopy}`);
 };
+
+mergeSort([11,2,5,9,43]);
